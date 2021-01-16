82ab478d328d44b7c14279f7c1fdd18f4ad0c8b7
KK964 <morrkade05@gmail.com>
2021-01-15 19:24:17 -0600

    Added GUNS scoreboard

--- src/main/java/net/justminecraft/minigames/gungame/JustGunsGame.java
+++ src/main/java/net/justminecraft/minigames/gungame/JustGunsGame.java
@@ -5,8 +5,25 @@ import net.justminecraft.minigames.minigamecore.Minigame;
 
 public class JustGunsGame extends Game {
     public int taskId = 0;
+    Scoreboard scoreboard;
+    HashMap<Player, Integer> playerScore = new HashMap<>();
+    HashMap<Player, Integer> killStreak = new HashMap<>();
+    HashMap<Player, Integer> playerKills = new HashMap<>();
 
     public JustGunsGame(Minigame mg) {
         super(mg);
+        justguns = (JustGuns) mg;
+        scoreboard = Bukkit.getScoreboardManager().getNewScoreboard();
+    }
+
+    public  void updateScore(Player p) {
+        scoreboard.resetScores(ChatColor.RED + p.getName() + ": " + ChatColor.DARK_RED + (playerKills.get(p) - 1));
+        scoreboard.resetScores(ChatColor.RED + p.getName() + ": " + ChatColor.DARK_RED + (playerKills.get(p)));
+        Game g = MG.core().getGame(p);
+        if(g != null && g.minigame == justguns) {
+            scoreboard.getObjective(DisplaySlot.SIDEBAR).getScore(ChatColor.RED + p.getName() + ": " + ChatColor.DARK_RED + playerKills.get(p)).setScore(3);
+        } else {
+            scoreboard.getObjective(DisplaySlot.SIDEBAR).getScore(ChatColor.RED + p.getName() + ": " + ChatColor.DARK_RED + "✗").setScore(3);
+        }
     }
 }
